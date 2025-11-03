d790f82a5d2a3017e54cfc57a692dc5d79569300
Denis <denis.bernardes099@gmail.com>
2025-11-03 11:28:27 -0300

    fix bug initial configuration

--- S4ACS/S4ACS files/python/main.py
+++ S4ACS/S4ACS files/python/main.py
@@ -15,27 +15,27 @@ from utils import (
 
 def main(night_dir, file, data, tuple_header_jsons, log_file):
     error_json = {"status": False, "code": 0, "source": ""}
-    try:
-        dict_header_jsons = {k: v for (k, v) in zip(sub_systems, tuple_header_jsons)}
-        data = np.asarray(data, dtype=np.uint16)
-        file = os.path.join(night_dir, file)
+    # try:
+    dict_header_jsons = {k: v for (k, v) in zip(sub_systems, tuple_header_jsons)}
+    data = np.asarray(data, dtype=np.uint16)
+    file = os.path.join(night_dir, file)
 
-        for cls in [Focuser, S4ICS, S4GUI, TCS, Weather_Station, General_KWs, CCD]:
-            obj = cls(dict_header_jsons, log_file)
-            obj.fix_keywords()
-            hdr = obj.hdr
-        obj.reset_header()
-        data = fix_image_orientation(hdr["CHANNEL"], hdr["EMMODE"], data)
-        file = verify_file_already_exists(file)
-        hdu = fits.PrimaryHDU(data, hdr)
-        hdu.header["BZERO"] = (32768, "Zero point in scaling equation")
-        hdu.header["BSCALE"] = (1, "Linear factor in scaling equation")
-        hdu.header["NAXIS1"] = (hdu.header["NAXIS1"], "Number of columns")
-        hdu.header["NAXIS2"] = (hdu.header["NAXIS2"], "Number of rows")
-        hdu.writeto(file, output_verify="ignore")
-        return json.dumps(error_json)
-    except Exception as e:
-        error_json["status"] = True
-        error_json["code"] = 1
-        error_json["source"] = repr(e)
-        return json.dumps(error_json)
+    for cls in [Focuser, S4ICS, S4GUI, TCS, Weather_Station, General_KWs, CCD]:
+        obj = cls(dict_header_jsons, log_file)
+        obj.fix_keywords()
+        hdr = obj.hdr
+    obj.reset_header()
+    data = fix_image_orientation(hdr["CHANNEL"], hdr["EMMODE"], data)
+    file = verify_file_already_exists(file)
+    hdu = fits.PrimaryHDU(data, hdr)
+    hdu.header["BZERO"] = (32768, "Zero point in scaling equation")
+    hdu.header["BSCALE"] = (1, "Linear factor in scaling equation")
+    hdu.header["NAXIS1"] = (hdu.header["NAXIS1"], "Number of columns")
+    hdu.header["NAXIS2"] = (hdu.header["NAXIS2"], "Number of rows")
+    hdu.writeto(file, output_verify="ignore")
+    #     return json.dumps(error_json)
+    # except Exception as e:
+    #     error_json["status"] = True
+    #     error_json["code"] = 1
+    #     error_json["source"] = repr(e)
+    #     return json.dumps(error_json)
