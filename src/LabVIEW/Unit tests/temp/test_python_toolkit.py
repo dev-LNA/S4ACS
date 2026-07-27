0c5817e98db739556c5b42c4ad718e679d6188a7
Denis <denis.bernardes099@gmail.com>
2026-07-27 16:28:28 -0300

    fix labview tests

--- src/LabVIEW/Unit tests/temp/test_python_toolkit.py
+++ src/LabVIEW/Unit tests/temp/test_python_toolkit.py
@@ -1,19 +1,9 @@
 import json
 
-sub_systems = [
-    "CCD",
-    "S4GUI",
-    "S4ICS",
-    "TCS",
-    "FOCUSER",
-    "WSTATION",
-    "GENERAL KW",
-]
 
-
-def main(file, data, tuple_header_jsons) -> str:
+def main(file, data, dict_header_jsons) -> str:
     error_json = {"status": False, "code": 0, "source": ""}
-    header_jsons = {k: v for (k, v) in zip(sub_systems, tuple_header_jsons)}
+    header_jsons = json.loads(dict_header_jsons)
 
     if file != "20251007_s4c1_000139.fits":
         raise ValueError(f"Wrong file name value: {file}")
